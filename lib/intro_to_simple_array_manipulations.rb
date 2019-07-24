6cc8ff7101236b3c14781f8476d3d6feaee92d6f
Eric Loudon <loudon.eric@gmail.com>
2019-07-24 11:19:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,16 +10,3 @@ def using_unshift(array, string)
 bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
-def using_pop(array)
-continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-deleted_string = continents.pop()
-p continents
-p deleted_string
-end
-
-def pop_with_args(array)
-  dog_breeds =  ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-small_breeds = dog_breeds.pop(2)
-p dog_breeds
-p small_breeds
-end
