07812481f2562f4cabb36932e57ec948d2979fd0
Eric Loudon <loudon.eric@gmail.com>
2019-07-24 11:13:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,7 +12,7 @@ end
 
 def using_pop(array, string)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-continents.pop
+contiinents.pop
 end
 
 def pop_with_args(array)
